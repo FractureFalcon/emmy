fc93bd50b907d3ec9b508b8e77eec69718d4fe5d
Kate Reinhard <kate@katereinhard.com>
2016-08-07 12:43:03 -0700

    Modified emmy's text a bit and the final app screen text.

--- emmy-outro-data.js
+++ emmy-outro-data.js
@@ -1,17 +1,19 @@
 var introString = "Welcome back, $user. How're you feeling?";
 var textStrings = [
     "I hope you're hanging in there alright. Don't be afraid to talk out your feelings, now.",
-    "I want you to know you've done great thus far. You're making great progress. Please keep talking to me for as long as you'd like to. I apologize in advance if I get a little reptitive. Just say \"I'm done with treatment\" to end the treatment."
+    "I want you to know you've done great thus far. You're making great progress. Please keep talking to me for as long as you'd like to. I apologize in advance if I get a little reptitive. Just say \"I'm done with treatment\" to end the treatment.",
+    "I hope I've helped you today. Feel free to come back anytime. I'll always be here"
 ];
 var startLoopIndex = 0;
 
 function updateCustomLogic(userText) {
-    if(stringIndex > textStrings.length - 1) {
-        if(compareString('I\'m done with treatment', userText) > .7) {
-            loadFinal();
-        }
-        else {
+    if(stringIndex === textStrings.length - 1) {
+        if(compareString('I\'m done with treatment', userText) <= .7) {
             stringIndex = 0;
         }
+    }
+    
+    if(stringIndex > textStrings.length - 1) {
+        loadFinal();
     }
 }
