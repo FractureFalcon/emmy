3250d8c244e06c4b5f6e8c8e8be0e5a2ad41479c
Kate Reinhard <kate@katereinhard.com>
2016-11-14 21:31:56 -0800

    Emmy uses user's name more often.

--- emmy-intro-data.js
+++ emmy-intro-data.js
@@ -1,10 +1,10 @@
 var introString = "Hi! My name is Emmy, named after EMDR. I'm here to help you with the EMDR Treatment. The EMDR treatment is designed to help you move forward from memories that hold you back. What's your name?";
 var textStrings = [
     "Nice to meet you, $user!  Now, I know this can be difficult, but for the treatment to work, I need you to relive the memory you want to move past. Really envision it. So, take a moment, and when you feel up to it, tell me about it. I'll wait here as long as I need to.",
-    "How did you feel about that?",
+    "How did you feel about that, $user?",
     "That sounds like it was tough, $user... Let's try talking about it a different way. What positive qualities do you think you displayed there?",
-    "Good! Now let's try reframing the whole situation. Describe the situation again, but this time only describing yourself positively!",
-    "That's great! Now, we can do this again (respond \"Again\") or we can proceed to the EMDR part of the treatment (respond \"I'm ready\"). Either one is okay, take your time deciding, okay?",
+    "Good! Now try reframing the whole situation, $user. Describe the situation again, but this time only describing yourself positively!",
+    "That's great, $user! Now, we can do this again (respond \"Again\") or we can proceed to the EMDR part of the treatment (respond \"I'm ready\"). Either one is okay, take your time deciding, okay?",
     "Okay, when you hit ENTER again, you'll see a big glowy dot moving around the screen. Concentrate on the positive description of the events you've told me about, and follow the dot with your eyes.",
     "Here we go!"
 ];
--- emmy-outro-data.js
+++ emmy-outro-data.js
@@ -1,8 +1,8 @@
 var introString = "Welcome back, $user. How're you feeling?";
 var textStrings = [
-    "I hope you're hanging in there alright. Don't be afraid to talk out your feelings, now.",
-    "I want you to know you've done great thus far. You're making great progress. Please keep talking to me for as long as you'd like to. Just say \"I'm done with treatment\" to end the treatment.",
-    "I hope I've helped you today. Feel free to come back anytime. I'll always be here"
+    "I hope you're hanging in there alright. Don't be afraid to talk about your feelings, $user.",
+    "I want you to know you're making great progress, $user. Please keep talking to me for as long as you'd like to. Just say \"I'm done with treatment\" to end the treatment.",
+    "I hope I've helped you today, $user. Feel free to come back anytime. I'll always be here"
 ];
 var startLoopIndex = 0;
 
